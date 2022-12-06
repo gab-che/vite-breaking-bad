f03a3cd6b1d3c5a4d0ac9561b66387de12fd3b2d
gab-che <gabriellachessa1@gmail.com>
2022-12-06 17:30:36 +0100

    prints simple character list

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -19,10 +19,18 @@
 </script>
 
 <template>
+    <ul class="list-unstyled character_list">
+        <li v-for="character in store.charachterList">
+            {{character.name}}
+        </li>
+    </ul>
     <SingleCharacter></SingleCharacter>
 </template>
 
 <style scoped lang="scss">
     @use '../styles/general.scss' as *;
     @use '../styles/partials/variables' as *;
+    .character_list{
+        color: $secondary_color;
+    }
 </style>
