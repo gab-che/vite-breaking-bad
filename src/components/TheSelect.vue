cd8a3df088bf17ade2f2b0f672a4d2ae86b1392f
gab-che <gabriellachessa1@gmail.com>
2022-12-06 15:26:22 +0100

    imports bootstrap, adds empty components

---
+++ src/components/TheSelect.vue
@@ -0,0 +1,22 @@
+<script>
+    export default{
+        data(){
+            return{
+
+            }
+        },
+
+        methods:{
+
+        }
+    }
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+    @use '../styles/general.scss' as *;
+    @use '../styles/partials/variables' as *;
+</style>
